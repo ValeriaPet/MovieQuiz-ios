c81af51cfcf1a198261f2c72e3296bdf522eefed
Valeriia Petrosian <petrosyan.valeria@gmail.com>
2023-11-17 00:17:34 +0400

    Добавление тестов на кнопки да/нет + показание алерта

--- MovieQuizUITests/MovieQuizUITests.swift
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -18,9 +18,8 @@ final class MovieQuizUITests: XCTestCase {
         app.launch()
         
         continueAfterFailure = false
-        
     }
-
+    
     override func tearDownWithError() throws {
         try super.tearDownWithError()
         
@@ -28,14 +27,56 @@ final class MovieQuizUITests: XCTestCase {
         app = nil
     }
     
-    func testScreenCast() throws {
-        
-    }
     func testExample() throws {
         
         let app = XCUIApplication()
         app.launch()
-
+    }
+    
+    func testYesButton() {
+        sleep(3)
         
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testNoButton() {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+
+        let indexLabel = app.staticTexts["Index"]
+       
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testAlert() {
+        sleep(2)
+        for _ in 1...10 {
+            app.buttons["No"].tap()
+            sleep(2)
+        }
+        let alert = app.alerts["Game results"]
+        
+        XCTAssertTrue(alert.exists)
     }
 }
