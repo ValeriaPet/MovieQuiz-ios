c64a254ca008e063814e9f3d5663861dd76636f2
Valeriia Petrosian <petrosyan.valeria@gmail.com>
2023-11-25 03:07:28 +0400

    Создание PresenterProtocol + TestPresenter

---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by LERÄ on 25.11.23.
+//
+
+import Foundation
---
+++ MovieQuizTests/PresenterTest.swift
@@ -0,0 +1,8 @@
+//
+//  PresenterTest.swift
+//  MovieQuizTests
+//
+//  Created by LERÄ on 25.11.23.
+//
+
+import Foundation
